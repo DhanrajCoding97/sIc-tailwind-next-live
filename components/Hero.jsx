24acfb32cafe808c52f0b1efb1e19c0246cb24c5
Dhanraj balkrishna shetty <dhanrajCoding@gmail.com>
2022-09-28 02:02:11 +0530

    initial commit

---
+++ components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      {/* Overlay */}
+      <div />
+      <div>
+        <h1>
+          If you want to get Electrical or Instrumentation Products you have
+          come to the Right place.
+        </h1>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
