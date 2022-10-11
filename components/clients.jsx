50d1e913802a870fcf0a179caf111bdcd1884e13
Dhanraj balkrishna shetty <dhanrajCoding@gmail.com>
2022-10-11 19:29:12 +0530

    :updated cards styling

--- components/clients.jsx
+++ components/clients.jsx
@@ -3,15 +3,24 @@ import { motion } from "framer-motion";
 const Clients = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div id="clients" className="bg-white p-4 font-bold w-80">
+    <div id="clients" className="bg-white font-bold w-80">
       <motion.div
         transition={{ layout: { duration: 1, type: "spring" } }}
         layout
         onClick={() => setIsOpen(!isOpen)}
+        whileHover={{
+          scale: 1.2,
+          backgroundColor: "#34d399",
+          transition: { duration: 2 },
+        }}
+        whileTap={{
+          scale: 0.9,
+        }}
       >
-        <motion.h1 layout="position" className="text-2xl text-black font-bold">
+        <motion.h1 layout="position" className="text-black text-4xl font-bold">
           Emerson
         </motion.h1>
+        <motion.a className="text-black font-bold">Read More</motion.a>
         {isOpen && (
           <motion.div className="pt-4">
             <p className="leading-relaxed text-black">
@@ -21,7 +30,6 @@ const Clients = () => {
               industrial, commercial, and consumer markets. Emerson has
               approximately 86,700 employees and 170 manufacturing locations
             </p>
-            <motion.a>Read More</motion.a>
           </motion.div>
         )}
       </motion.div>
